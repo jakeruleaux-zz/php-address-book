ea805774f9a59b9f9c9166276dffc66b88e1e7ef
jacob ruleaux <jakeruleaux@gmail.com>
2017-06-30 08:39:04 -0700

    build boilerplate for model file

---
+++ src/Address.php
@@ -0,0 +1,62 @@
+<?php
+    class contact
+    {
+        private $name;
+        private $address;
+        private $phone;
+
+        function __construct()
+        {
+            $this->name;
+            $this->address;
+            $this->phone;
+        }
+
+        function setName()
+        {
+
+        }
+
+        function getName()
+        {
+
+        }
+
+        function setAddress()
+        {
+
+        }
+
+        function getAddress()
+        {
+
+        }
+
+        function setPhone()
+        {
+
+        }
+
+        function getPhone()
+        {
+
+        }
+
+        function save()
+
+
+
+        static function getAll()
+
+
+        static function deleteAll()
+    }
+
+
+
+
+
+
+
+
+ ?>
