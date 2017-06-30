65682670b070173f99dc0dc601c64863603b1585
jacob ruleaux <jakeruleaux@gmail.com>
2017-06-30 08:54:58 -0700

    finish model file with class, constructor and session functions

--- src/Address.php
+++ src/Address.php
@@ -12,51 +12,50 @@
             $this->phone;
         }
 
-        function setName()
+        function setName($new_name)
         {
-
+            $this->name = $new_name
         }
 
         function getName()
         {
-
+            return $this->name;
         }
 
-        function setAddress()
+        function setAddress($new_address)
         {
-
+            $this->address = $new_address;
         }
 
         function getAddress()
         {
-
+            return $this->address;
         }
 
-        function setPhone()
+        function setPhone($new_phone)
         {
-
+            $this->phone = $new_phone;
         }
-
         function getPhone()
         {
-
+            return $this->phone;
         }
 
         function save()
-
-
+        {
+            array_push($_SESSION['list_of_contacts'], $this);
+        }
 
         static function getAll()
-
+        {
+            return $_SESSION['list_of_contacts'];
+        }
 
         static function deleteAll()
+        {
+            return $_SESSION['list_of_contacts'] = array();
+        }
     }
 
 
-
-
-
-
-
-
- ?>
+?>
